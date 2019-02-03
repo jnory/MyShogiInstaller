94ee96f6f982cfba761891c1e671373032d6325d
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2019-02-04 00:19:21 +0900

    fix args

--- macOS/install.sh
+++ macOS/install.sh
@@ -137,8 +137,9 @@ if [ ${START_FROM} -le 2 ]; then
 fi
 
 if [ ${START_FROM} -le 3 ]; then 
-    build_yaneuraou ${PREFIX} ${YANEURAOU_REPOS} ${YANEURAOU_VERSION} ${ROOTDIR}/MacOS ${BASEDIR}/engine_defines
+    build_yaneuraou ${PREFIX} ${YANEURAOU_REPOS} ${YANEURAOU_VERSION} ${ROOTDIR}/MacOS ${BASEDIR}/engine_defines ${BASEDIR}/common/YaneuraOu.patch
 fi
+exit
 
 if [ ${START_FROM} -le 4 ]; then 
     build_soundplayer ${PREFIX} ${SOUNDPLAYER_REPOS} ${SOUNDPLAYER_VERSION} macos macOS ${ROOTDIR}/MacOS dylib
